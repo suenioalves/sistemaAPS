8b5647409675a4596490d816cf106733769e380c
Suênio Trindade Alves <sueniotrindade@gmail.com>
2025-07-26 06:24:22 -0500

    orientacoes pro git

--- bd_sistema_aps/Scripts/PRENATAL/TOTAL DE GRAVIDEZ PLANEJADAS.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-    COUNT(DISTINCT tappn.co_unico_pre_natal) AS total_partos
-FROM 
-    tb_atend_prof_pre_natal tappn
-WHERE 
-    tappn.st_gravidez_planejada IS NOT NULL;
--- bd_sistema_aps/Scripts/Zoutros/PROCURAR O NOME DE COLUNA.sql
+++
@@ -1,3 +0,0 @@
-SELECT table_schema, table_name, column_name
-FROM information_schema.columns
-WHERE column_name ILIKE '%co_proced%';
